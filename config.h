d6d89cf12b875e5ebebc6d6cf28420c5fb007238
Azayel <Asayel404@proton.me>
2023-11-29 22:06:25 +0100

    Added Playback buttons to controll Media Playback

--- config.h
+++ config.h
@@ -76,13 +76,18 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
+/*Controll Media Player commands*/
+static const char *mediapausecmd[] = {"playerctl", "play-pause", NULL};
+static const char *medianextcmd[] = {"playerctl", "next", NULL};
+static const char *mediaprevcmd[] = {"playerctl", "previous", NULL};
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_Left,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Right,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
@@ -101,6 +106,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+  { MODKEY|ShiftMask,  XK_Left, spawn, {.v = mediaprevcmd }},
+  { MODKEY|ShiftMask,  XK_p, spawn, {.v = mediapausecmd }},
+  { MODKEY|ShiftMask,  XK_Right, spawn, {.v = medianextcmd }},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
