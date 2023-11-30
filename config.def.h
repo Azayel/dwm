0069e0aff215cd1ac48a7a74d2af4d2dc11a248f
Azayel <Asayel404@proton.me>
2023-11-30 23:29:16 +0100

    Added Print Screen And Lock Screen

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-
+#include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 15;        /* gaps between windows */
@@ -81,6 +81,12 @@ static const char *mediapausecmd[] = {"playerctl", "play-pause", NULL};
 static const char *medianextcmd[] = {"playerctl", "next", NULL};
 static const char *mediaprevcmd[] = {"playerctl", "previous", NULL};
 
+/*Lock Screen Command*/
+static const char *lockscreencmd[] = {"i3lock_launch", NULL, NULL};
+
+/*Make Screenshot Flameshot*/
+static const char *screenshotcommand[] = {"flameshot", "gui", NULL};
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -106,9 +112,11 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-  { MODKEY|ShiftMask,  XK_Left, spawn, {.v = mediaprevcmd }},
-  { MODKEY|ShiftMask,  XK_p, spawn, {.v = mediapausecmd }},
-  { MODKEY|ShiftMask,  XK_Right, spawn, {.v = medianextcmd }},
+  { MODKEY|ShiftMask,  		XK_Left,   spawn,          {.v = mediaprevcmd }},
+  { MODKEY|ShiftMask, 	 	XK_p,      spawn,          {.v = mediapausecmd }},
+  { MODKEY|ShiftMask,  		XK_Right,  spawn,          {.v = medianextcmd }},
+	{ MODKEY|ShiftMask,		XK_l,	   spawn,  {.v = lockscreencmd}},
+	{               0,    XK_Print, spawn, {.v = screenshotcommand}},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
