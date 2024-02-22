e1441faeb84f53c8e92d2bcdd9c41894815934a5
Azayel <Asayel404@proton.me>
2024-02-22 18:59:00 +0100

    Added a script which checks the amount of updates avaiable

---
+++ scripts/dwmupdates.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Set the interval for updates (in minutes)
+update_interval=2
+
+# File to store the last update time
+last_update_file="/tmp/last_update_time"
+last_update_file_amount="/tmp/last_update_amount"
+# Get the current minute
+current_minute=$(date +%M)
+
+# Check if the last update time file exists
+if [ -e "$last_update_file" ]; then
+    # Read the last update time from the file
+    last_update_minute=$(cat "$last_update_file")
+
+    # Calculate the difference in minutes
+    time_difference=$((current_minute - last_update_minute))
+
+    # Check if the difference is greater than or equal to the update interval
+    if [ "$time_difference" -ge "$update_interval" ]; then
+        # Execute the updates
+        updatesarch="$(checkupdates 2> /dev/null | wc -l)"
+        updatesaur="$(yay -Qum 2> /dev/null | wc -l)"
+        updates=$(("$updatesarch" + "$updatesaur"))
+        icon="🛠️"
+        echo "$updates" > "$last_update_file_amount"
+
+        # Update the last update time in the file
+        echo "$current_minute" > "$last_update_file"
+        
+        printf "%s %s \\n"  "$icon" "$updates"
+    else
+      icon="󰮯"
+      update_amount=$(cat "$last_update_file_amount")
+      printf "%s %s \\n"  "$icon" "$update_amount"
+    fi
+else
+    # If the file doesn't exist, create it and store the current minute
+    echo "$current_minute" > "$last_update_file"
+    echo "0" > "$last_update_file_amount" 
+fi
+
